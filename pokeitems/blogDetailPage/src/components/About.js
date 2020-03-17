ce632840f12f56660d4c47e6ab17b58fa6e40442
Chaser90 <54658653+Chaser90@users.noreply.github.com>
2020-03-17 17:11:16 +0900

    Add files via upload

--- pokeitems/blogDetailPage/src/components/About.js
+++ pokeitems/blogDetailPage/src/components/About.js
@@ -1,1 +1,12 @@
+import React from 'react'
+import Rainbow from '../hoc/Rainbow'
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore adipisci, accusantium quarerat et autem necessitatibus hic harum. Tenetur asperiores at nihil, earum recusandaem aliquid repellendus, veniam eveniet modimolestias sequi.</p>
+        </div>
+    )
+}
 
+export default Rainbow(About)
---
+++ pokeitems/blogDetailPage/src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div className="container">
+            <h4 className="center">contact </h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore adipisci, accusantium quarerat et autem necessitatibus hic harum. Tenetur asperiores at nihil, earum recusandaem aliquid repellendus, veniam eveniet modimolestias sequi.</p>
+        </div>
+    )
+}
+
+export default Contact
