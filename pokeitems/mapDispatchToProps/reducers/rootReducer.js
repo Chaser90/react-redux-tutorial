a07ffe3787f7e7e7da8a668fc651d43befa52b83
Chaser90 <54658653+Chaser90@users.noreply.github.com>
2020-03-17 17:24:47 +0900

    Add files via upload

--- pokeitems/mapDispatchToProps/reducers/rootReducer.js
+++ pokeitems/mapDispatchToProps/reducers/rootReducer.js
@@ -1,1 +1,22 @@
+const initState = {
+    posts: [
+        {id: '1', title: 'Squirtle Laid an Egg', body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore adipisci, accusantium quarerat et autem necessitatibus hic harum. Tenetur asperiores at nihil, earum recusandaem aliquid repellendus, veniam eveniet modimolestias sequi.'},
+        {id: '2', title: 'Charmander Laid an Egg', body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore adipisci, accusantium quarerat et autem necessitatibus hic harum. Tenetur asperiores at nihil, earum recusandaem aliquid repellendus, veniam eveniet modimolestias sequi.'},
+        {id: '3', title: 'a Helix Fossil was Found', body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore adipisci, accusantium quarerat et autem necessitatibus hic harum. Tenetur asperiores at nihil, earum recusandaem aliquid repellendus, veniam eveniet modimolestias sequi.'}
+    ]
+}
 
+const rootReducer = (state = initState, action) => {
+    if (action.type === 'DELETE_POST') {
+        let newPosts = state.posts.filter(post => {
+            return action.id !== post.id
+        });
+        return {
+            ...state,
+            posts: newPosts
+        }
+    }
+    return state;
+}
+
+export default rootReducer
