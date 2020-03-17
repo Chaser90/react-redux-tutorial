38e1c7514b042607cef304c74ed73f4dc622899d
Chaser90 <54658653+Chaser90@users.noreply.github.com>
2020-03-17 17:18:51 +0900

    Add files via upload

---
+++ pokeitems/formsRevisited/App.js
@@ -0,0 +1,25 @@
+import React , { Component } from 'react';
+import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
+
+class App extends Component {
+  state = {
+    ninjas : [
+      { name:'Ryu', age:'32', belt:'black', id: 1 },
+      { name:'Hyun', age:'25', belt:'yellow', id: 2 },
+      { name:'yeon', age:'27', belt:'silver', id: 3 }
+    ]
+  }
+  render() {
+  return (
+    <div className="App">
+      <h1> My Second React app!</h1>
+      <p>Welcome :)</p>
+      <Ninjas ninjas={this.state.ninjas } />
+      <AddNinja />
+    </div>
+    )
+  }
+}
+
+export default App;
